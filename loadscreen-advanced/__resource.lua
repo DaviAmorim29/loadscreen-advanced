c0f096203c53bd9530d89cf03b74d1c398cca0d3
Amorim <daviamorim29@hotmail.com>
2019-01-27 04:25:33 -0200

    Add files via upload

---
+++ loadscreen-advanced/__resource.lua
@@ -0,0 +1,20 @@
+Description "Amorim-Loadscreen"
+
+files {
+-- Site em si -- 
+	"index.html",
+	"css/main.css",
+	-- imagens --
+	"img/logo.png",
+	"img/person-male.png",
+	"img/cursor.png",
+	-- javascript --
+	"js/music-controls.js",
+	"js/music-handler.js",
+	"js/main.js",
+	"js/config.js",
+	"js/synn.js",
+	"js/progressbar-renderer.js",
+}
+
+loadscreen "index.html"
