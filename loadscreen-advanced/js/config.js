3fa2d7acd9f1d518de11f04bc67b5b0a2b775249
Amorim <daviamorim29@hotmail.com>
2019-01-27 04:26:37 -0200

    Update config.js

--- loadscreen-advanced/js/config.js
+++ loadscreen-advanced/js/config.js
@@ -1,1 +1,20 @@
+/* edit chang7 - EliteBR // edit: amorim#0567 synn screen */
 
+var config =
+{    
+
+    cursorImage: "img/cursor.png",
+
+    imageSize: [578, 236],
+    
+    music:
+    [
+        "FmZisnTvN_M",
+        "TQ5DUv_ZwRg",
+        "RL4qWNx7BS8",
+    ],
+
+    enableMusic: true,
+ 
+    musicVolume: 40,
+}
